d18f6ad9d55bf2866c9a55ee5ce20c6098d9f480
maxIterate <maxipp042@gmail.com>
2023-06-28 21:13:23 -0300

    restructured carousel, and hero/maintop is responsive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,32 +6,60 @@ import Navbar from "../components/Navbar";
 
 
 function HomePage() {
+    const mainTopTitle = "Discover the universe with Astrotravels"
+
+    const mainTopText = "Explore amazing destinations under the starry cloak. Immerse yourself in the celestial beauty as you unlock the secrets of the universe. AstroTravels takes you on unique stargazing experiences in magical destinations. Get ready for an unforgettable journey into infinity!"
+
+
+
     return (
         <div className="w-100 h-[100vh] bg-cover bg-no-repeat bg-bottom relative"
         style={{ backgroundImage: 'url(./src/assets/images/bg-2.jpg)' }}
         >
             <Navbar />
-            <div className="flex h-[68vh] px-10 pt-20">
-                <div className="w-2/3">
-                    <p className="text-white text-2xl md:text-4xl lg:text-6xl font-bold pr-20">Discover the universe with Astrotravels</p>
-                    <p className="text-white text-l md:text-xl lg:text-2xl pr-20 pt-10">Explore amazing destinations under the starry cloak. Immerse yourself in the celestial beauty as you unlock the secrets of the universe. AstroTravels takes you on unique stargazing experiences in magical destinations. Get ready for an unforgettable journey into infinity!</p>
+            <div className="h-[68vh] 
+            pt-20 
+            flex flex-col sm:flex-row
+            ">
+                <div className="
+                w-full md:w-2/3 max-h-min
+                sm:pl-10
+                px-10 sm:px-0
+                pb-10 sm:pb-0
+                ">
+                    <p className="text-white text-2xl md:text-4xl lg:text-4xl font-bold 
+                    pr-0 sm:pr-20 text-center sm:text-left
+                    ">{mainTopTitle}</p>
+                    <p className="text-white text-l md:text-xl lg:text-2xl 
+                    pr-0 sm:pr-20 text-center sm:text-left
+                    pt-10">{mainTopText}</p>
                 </div>
                 <div
                 className="
-                bg-blue-400
-                -skew-x-12
-                w-1/3
+                border-4 border-black rounded-none sm:rounded-l-full
+                w-full sm:w-3/4
                 h-full
-                absolute
-                right-10
-                top-0
-                bg-cover bg-no-repeat bg-bottom
-                z-0
-                border-x-4
+                bg-black
+                relative
                 "
-                style={{ backgroundImage: 'url(./src/assets/images/carousel-demo.jpg)' }}
                 >
+                    <div
+                    className="
+                    z-0
+                    w-full
+                    h-full
+                    rounded-l-full
+                    bg-cover bg-no-repeat bg-bottom
 
+                    hover:cursor-pointer hover:blur-sm
+                    transition-all
+                    "
+                    style={{ backgroundImage: 'url(./src/assets/images/carousel-demo.jpg)' }}>
+                        <p className="hover:blur-none bg-transparent z-20 
+                        absolute top-2/4 left-2/4
+                        hover:block hidden
+                        ">Show more</p>
+                    </div>
                 </div>
             </div>
             <div className="flex h-[20vh] relative justify-center items-center">
